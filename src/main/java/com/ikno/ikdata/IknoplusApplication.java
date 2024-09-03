aa61bf3b51ae3b4606fb25c85483f4c1a83141f8
WillianRu <correo.willianrueda@gmail.com>
2024-09-03 15:22:07 -0500

    Basic structure of the project, .gitignore file is edited

---
+++ src/main/java/com/ikno/ikdata/IknoplusApplication.java
@@ -0,0 +1,13 @@
+package com.ikno.ikdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IknoplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IknoplusApplication.class, args);
+	}
+
+}
